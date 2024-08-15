e832e79bf7c820fee9a71a5c51c169e9f7ae1ef1
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-08-15 23:45:17 +0100

    update: added convex to work with clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://correct-ferret-88.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ClerkProvider, SignInButton, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import {
+  Authenticated,
+  AuthLoading,
+  ConvexProvider,
+  ConvexReactClient,
+  Unauthenticated,
+} from "convex/react";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export function ConvexClientProvider({ children }: ConvexClientProviderProps) {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Unauthenticated>
+          <SignInButton />
+        </Unauthenticated>
+        <Authenticated>{children}</Authenticated>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
