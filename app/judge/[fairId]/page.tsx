28074cf5504d495671baed0f35bc2cb95ef8b8a7
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-08-19 08:01:07 +0100

    update: styles

--- app/judge/[fairId]/page.tsx
+++ app/judge/[fairId]/page.tsx
@@ -11,7 +11,7 @@ const FairPage = ({ params }: { params: { fairId: string } }) => {
     id: params.fairId as Id<"fairs">,
   });
 
-  const deleteFair = useMutation(api.fairs.deleteFair)
+  const deleteFair = useMutation(api.fairs.deleteFair);
 
   const onDelete = async () => {
     deleteFair({ id: params.fairId as Id<"fairs"> });
@@ -20,21 +20,35 @@ const FairPage = ({ params }: { params: { fairId: string } }) => {
 
   const router = useRouter();
   return (
-    <div>
-      <div className="flex space-x-3">
-        <Button onClick={() => router.push(`/judge/edit-fair/${params.fairId}`)}>Edit</Button>
-        <Button onClick={onDelete} variant="destructive">Delete</Button>
+    <div className="max-w-4xl mx-auto">
+      <div className="flex space-x-3 mt-5 justify-end">
+        <Button
+          onClick={() => router.push(`/judge/edit-fair/${params.fairId}`)}
+        >
+          Edit
+        </Button>
+        <Button onClick={onDelete} variant="destructive">
+          Delete
+        </Button>
       </div>
 
       {fair?.map((item) => (
-        <div key={item._id}>
+        <div key={item._id} className="mt-5">
+          <h1 className="font-bold break-normal text-3xl md:text-5xl">
+            {item.title}
+          </h1>
+          <h3 className="italic text-gray-500">
+            {item.subtitle}
+          </h3>
+          <p className="mt-2 mb-2 text-sm">Deadline: {item.deadline}</p>
           <img src={item.imageUrl} />
-          <p>{item.title}</p>
-          <p>{item.subtitle}</p>
-          <p>{item.deadline}</p>
+          <h2 className="my-6 font-semibold text-xl">About</h2>
           <p>{item.about}</p>
+          <h2 className="my-6 font-semibold text-xl">Requirements</h2>
           <p>{item.requirements}</p>
+          <h2 className="my-6 font-semibold text-xl">Prices</h2>
           <p>{item.prices}</p>
+          <h2 className="my-6 font-semibold text-xl">Judging Criteria</h2>
           <p>{item.judgingCriteria}</p>
         </div>
       ))}
