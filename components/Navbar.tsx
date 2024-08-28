ad7a42c26941fdb48ad1fe487fc1a72a4262c40a
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-08-28 09:08:02 +0100

    update: about

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,11 +9,8 @@ const Navbar = () => {
   return (
     <div className="px-2 max-w-[1400px] mx-auto">
       <div className="flex items-center justify-between py-8 lg:py-4">
-        <Link
-          href="/"
-          className="font-semibold font-serif md:text-md lg:text-lg"
-        >
-          Hack<span className="text-green-600">n</span>Fair
+        <Link href="/" className="font-semibold font-serif md:text-lg">
+          Hack<span className="text-[#4EB645]">n</span>Fair
         </Link>
         <nav>
           <section className="flex lg:hidden">
@@ -32,22 +29,22 @@ const Navbar = () => {
                 <X />
               </div>
               <div className="flex flex-col items-center justify-between min-h-[250px] w-full px-3 uppercase">
-                <Link href="/" className="hover:text-green-600 font-[500]">
+                <Link href="/" className="hover:text-[#4EB645] font-[500]">
                   Home
                 </Link>
-                <Link href="/fairs" className="hover:text-green-600 font-[500]">
+                <Link href="/fairs" className="hover:text-[#4EB645] font-[500]">
                   Fairs
                 </Link>
                 <Link
                   href="/dashboard"
-                  className="hover:text-green-600 font-[500]"
+                  className="hover:text-[#4EB645] font-[500]"
                 >
                   Dashboard
                 </Link>
-                <Link href="/about" className="hover:text-green-600 font-[500]">
+                <Link href="/about" className="hover:text-[#4EB645] font-[500]">
                   About
                 </Link>
-                <Button className="bg-green-600 hover:bg-green-500 w-full text-base">
+                <Button className="bg-[#4EB645] hover:bg-[#60e454] w-full text-base">
                   Create Fair?
                 </Button>
               </div>
@@ -56,22 +53,22 @@ const Navbar = () => {
 
           <div className="hidden lg:flex justify-between items-center py-6 px-4">
             <div className="flex space-x-5 items-center">
-              <Link href="/" className="hover:text-green-600 font-[500]">
+              <Link href="/" className="hover:text-[#4EB645] font-[500]">
                 Home
               </Link>
-              <Link href="/fairs" className="hover:text-green-600 font-[500]">
+              <Link href="/fairs" className="hover:text-[#4EB645] font-[500]">
                 Fairs
               </Link>
               <Link
                 href="/dashboard"
-                className="hover:text-green-600 font-[500]"
+                className="hover:text-[#4EB645] font-[500]"
               >
                 Dashboard
               </Link>
-              <Link href="/about" className="hover:text-green-600 font-[500]">
+              <Link href="/about" className="hover:text-[#4EB645] font-[500]">
                 About
               </Link>
-              <Button className="bg-green-600 hover:bg-green-500 w-full text-base">
+              <Button className="bg-[#4EB645] hover:bg-[#60e454] w-full text-base">
                 Create Fair?
               </Button>
             </div>
--- components/unathenticated/Home.tsx
+++ components/unathenticated/Home.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import HomeImg from "@/assets/homeimg.jpg";
+import AboutImg from "@/assets/aboutimg.jpg";
 import { Button } from "../ui/button";
 import { SignInButton } from "@clerk/nextjs";
 
@@ -19,24 +20,49 @@ const Home = () => {
         </div>
 
         <div className="text-white h-full flex flex-col justify-center px-3 lg:px-10">
-          <h1 className="text-4xl pb-5 font-bold md:text-5xl lg:text-6xl">HacknFair</h1>
+          <h1 className="text-4xl pb-5 font-bold md:text-5xl lg:text-6xl">
+            HacknFair
+          </h1>
 
-          <p className="pr-2 md:text-md lg:text-lg">
+          <p className="pr-2 md:text-lg">
             Host science fair campaigns and showcase innovative student
             submissions
           </p>
 
           <SignInButton>
-            <Button className="bg-green-600 hover:bg-green-500 text-base rounded-none max-w-[250px] mt-10">
+            <Button className="bg-[#4EB645] hover:bg-[#60e454] text-base rounded-none max-w-[250px] mt-10">
               Sign In
             </Button>
           </SignInButton>
         </div>
       </div>
 
-
-      <div>
-        
+      <div className="px-3">
+        <div>
+          <p className="uppercase text-[#4EB645] font-bold mt-6">
+            Empowering young minds
+          </p>
+          <p className="uppercase font-bold text-lg mt-2">
+            Join us in celebrating innovation
+          </p>
+          <p className="mt-2">
+            <span className="font-semibold">HacknFair</span> is a versatile
+            online platform designed to host both science fairs and hackathons,
+            allowing users to create events, submit proposals, and engage in
+            voting and discussions.
+            <br />
+            What makes it unique is its dual functionality: it caters to
+            students, educators, and tech enthusiasts alike by combining the
+            excitement of science fairs with the competitive edge of hackathons.
+            <br />
+            Judges have dedicated tools to select winners, while the community
+            can participate through voting and comments, making it an inclusive
+            and interactive experience for all.
+          </p>
+        </div>
+        <div className="mt-7">
+            <Image src={AboutImg} alt="about" className="object-cover object-center" />
+        </div>
       </div>
     </div>
   );
