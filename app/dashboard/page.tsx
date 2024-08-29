512605e89f8bf7ba4792970b9dee361591c42fed
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-08-29 07:57:36 +0100

    for spring

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-import Link from "next/link";
-import React from "react";
+import React from 'react'
 
-const DashboardPage = () => {
-  const user = useQuery(api.users.getCurrentUser);
+const page = () => {
   return (
-    <div>
-      <p>Dashboard page</p>
-      <Link href={`/judge/${user?._id}/create-fair`}>
-        <Button className="m-10">Create fair</Button>
-      </Link>
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default DashboardPage;
+export default page
