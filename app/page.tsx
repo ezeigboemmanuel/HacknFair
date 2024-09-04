6bc9e9e5ee2387aaba708c02d44755100bc38cd2
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-04 08:38:34 +0100

    Try: refactor search code

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { api } from "@/convex/_generated/api";
 import { useMutation, useQuery } from "convex/react";
 import { marked } from "marked";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SyncLoader from "react-spinners/SyncLoader";
 
 interface HomeProps {
@@ -17,7 +17,11 @@ interface HomeProps {
 export default function Home({ searchParams }: HomeProps) {
   const store = useMutation(api.users.storeUser);
   const user = useQuery(api.users.getCurrentUser);
-  const fairs = useQuery(api.fairs.get, { search: searchParams.search });
+  const fairs = useQuery(api.fairs.get) || [];
+  const [searchText, setSearchText] = useState("");
+  const searchResult = useQuery(api.fairs.getSearch, {
+    search: searchParams.search,
+  });
 
   useEffect(() => {
     const storeUser = async () => {
@@ -42,21 +46,35 @@ export default function Home({ searchParams }: HomeProps) {
       </h1>
 
       <br />
-      {fairs?.length === 0 && <p>No fair found.</p>}
+      {searchResult?.length === 0 && <p>No fair found.</p>}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-5 w-full">
-        {fairs?.map((fair) => (
-          <div key={fair._id}>
-            <FairCard
-              link={fair._id}
-              imageUrl={fair.imageUrl}
-              title={fair.title}
-              subtitle={fair.subtitle}
-              deadline={fair.deadline}
-              userId={user?._id}
-              judgeId={fair.judgeId}
-            />
-          </div>
-        ))}
+        {searchParams
+          ? searchResult?.map((fair) => (
+              <div key={fair._id}>
+                <FairCard
+                  link={fair._id}
+                  imageUrl={fair.imageUrl}
+                  title={fair.title}
+                  subtitle={fair.subtitle}
+                  deadline={fair.deadline}
+                  userId={user?._id}
+                  judgeId={fair.judgeId}
+                />
+              </div>
+            ))
+          : fairs?.map((fair) => (
+              <div key={fair._id}>
+                <FairCard
+                  link={fair._id}
+                  imageUrl={fair.imageUrl}
+                  title={fair.title}
+                  subtitle={fair.subtitle}
+                  deadline={fair.deadline}
+                  userId={user?._id}
+                  judgeId={fair.judgeId}
+                />
+              </div>
+            ))}
       </div>
     </main>
   );
